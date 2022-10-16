791076dfe309b2098f6db3139b573b61385b22cb
OctopusLian <zoctopus@qq.com>
2022-10-16 14:35:14 +0800

    fix and close #2

--- src/main/java/com/imooc/oa/mapper/UserMapper.java
+++ src/main/java/com/imooc/oa/mapper/UserMapper.java
@@ -5,7 +5,7 @@ import com.imooc.oa.utils.MybatisUtils;
 
 public class UserMapper {
     public User selectByUsername(String username) {
-        User user = (User) MybatisUtils.executeQuery(sqlSession -> sqlSession.selectOne("username.selectByUsername"));
+        User user = (User) MybatisUtils.executeQuery(sqlSession -> sqlSession.selectOne("usermapper.selectByUsername", username));
         return user;
     }
 }
